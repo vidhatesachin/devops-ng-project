4fb5ae8b832eeeaadbd8a8c5699b9768af7c988f
Sachin Vidhate <sachin.vidhate@venturesunited.in>
2018-10-26 12:01:09 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
+import { UserService } from './user.service';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  encapsulation: ViewEncapsulation.None 
 })
 export class AppComponent {
-  title = 'my-dream-app';
+  title = 'devops angular project';
+  public users;
+  constructor(private userService: UserService) {
+    this.getUsers();
+  }
+  
+    getUsers() {
+       this.userService.getUsers().subscribe(
+          data => { this.users = data},
+          err => console.error(err),
+          () => console.log('done loading users')
+        );
+      }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { HttpClientModule } from '@angular/common/http';  
 import { AppComponent } from './app.component';
+import { UserService } from './user.service';
+import {DataTableModule} from "angular-6-datatable";
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    DataTableModule
   ],
-  providers: [],
+  providers: [UserService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
